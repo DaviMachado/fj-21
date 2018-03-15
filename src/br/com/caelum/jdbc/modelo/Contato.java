221c31cfd57bbb3d789a1a76ff47c74adcb714b4
Unknown <davisouzamachado@outlook.com>
2018-03-15 20:18:44 -0300

    Contato.java

---
+++ src/br/com/caelum/jdbc/modelo/Contato.java
@@ -0,0 +1,43 @@
+package br.com.caelum.jdbc.modelo;
+
+import java.util.Date;
+
+public class Contato {
+	private long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	private Date dataNascimento;
+	
+	// get's & set's
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+}
