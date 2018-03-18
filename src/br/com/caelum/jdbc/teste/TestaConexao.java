344b50c22f77fac5e612f37018542422758713cb
Unknown <davisouzamachado@outlook.com>
2018-03-18 19:28:01 -0300

    TestaConexao.java

---
+++ src/br/com/caelum/jdbc/teste/TestaConexao.java
@@ -0,0 +1,32 @@
+package br.com.caelum.jdbc.teste;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+
+import br.com.caelum.jdbc.ConnectionFactory;
+import br.com.caelum.jdbc.dao.ContatoDAO;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaConexao {
+	public static void main(String[] args) throws SQLException {
+//		Connection connection = new ConnectionFactory().getConnection();
+//		System.out.println("Conexão aberta!");
+//		connection.close();
+		
+		Contato contato = new Contato();
+		contato.setNome("Caelum");
+		contato.setEmail("Contato@caelum.com.br");
+		contato.setEndereco("R. Vergueiro 3185 cj57");
+//		contato.setDataNascimento(Calendar.getInstance());
+		
+		// gave nesssa conexão!!!
+		ContatoDAO dao = new ContatoDAO();
+		
+		// método elegante
+		dao.adiciona(contato);
+		
+		System.out.println("Gravado!");
+	}
+}
